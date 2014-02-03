98c4a0e252c35ad3e7b88fdfd61432ef20398ece
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-02-02 23:25:17 -0800

    Switch print_minmax to accept function pointer
    Send in the desired accumulate function

--- main.cpp
+++ main.cpp
@@ -86,30 +86,30 @@ void print_sol() {
   cout << '\n';
 }
 
-void print_minmax(const int iterations) {
-  vector <param> * spherical_sols_ptr = new vector <param>;
-  vector <param> * schwefel_sols_ptr = new vector <param>;
+void print_minmax(void (*accumulate)(vector <param> * solutions,
+				     const int iterations),
+		  const int iterations,
+		  const int scale,
+		  const string kind) {
+  vector <param> * solutions_ptr = new vector <param>;
 
-  accumulate_schwefel(schwefel_sols_ptr, iterations);
-  accumulate_spherical(spherical_sols_ptr, iterations);
+  accumulate(solutions_ptr, iterations);
 
-  auto spherical_minmax = minmax_element(spherical_sols_ptr->begin(),
-					 spherical_sols_ptr->end());
+  auto minmax = minmax_element(solutions_ptr->begin(),
+			       solutions_ptr->end());
 
-  auto schwefel_minmax = minmax_element(schwefel_sols_ptr->begin(),
-					schwefel_sols_ptr->end());
-
-  cout << "Spherical min\n" << *spherical_minmax.first/spherical_scale << '\n'
-       << "Spherical max\n" << *spherical_minmax.second/spherical_scale << '\n';
+  cout << kind+" min\n" << *minmax.first/scale << '\n'
+       << kind+" max\n" << *minmax.second/scale << '\n';
     
-  cout << "Schwefel min\n" << *schwefel_minmax.first/schwefel_scale << '\n'
-       << "Schwefel max\n" << *schwefel_minmax.second/schwefel_scale << '\n';
 }
 
 int main(int argc, char *argv[]) {
   srand(time(0));
 
-  print_minmax(1000000);
+  void (*accumulate)(vector <param> * solutions,
+		     const int iterations) = &accumulate_spherical;
+
+  print_minmax(accumulate, 10000, spherical_scale, "Spherical");
  
   return 0;
 }
