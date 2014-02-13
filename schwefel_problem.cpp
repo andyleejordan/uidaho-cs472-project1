c7b3fcd04ea1351bb4db9c0fe9e515e7f1f8cea0
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-02-12 21:44:46 -0800

    Schwefel updated for domain, range, const
    - Individual pointers

--- schwefel_problem.cpp
+++ schwefel_problem.cpp
@@ -1,13 +1,15 @@
 /* Copyright 2014 Andrew Schwartzmeyer
+ *
  * Source file for schwefel problem derived class
  */
 
 #include <cmath>
+
 #include "schwefel_problem.hpp"
 
-double Schwefel::problem(Individual subject) const {
+double Schwefel::problem(const Individual * subject) const {
   double sum = 0;
-  for (auto value : subject)
+  for (auto value : * subject)
     sum += value * std::sin(std::sqrt(std::abs(value)));
-  return 418.9829 * subject.size() + sum;
+  return 418.9829 * subject->size() + sum;
 }
--- schwefel_problem.hpp
+++ schwefel_problem.hpp
@@ -1,4 +1,5 @@
 /* Copyright 2014 Andrew Schwartzmeyer
+ *
  * Header file for base problem class
  */
 
@@ -9,17 +10,17 @@
 
 class Schwefel: public Problem {
 public:
-  Schwefel(const int r = 102400,
-	   const int s = 100,
-	   const double n = 0,
-	   const double x = 19000,
+  Schwefel(const double dn = -512.03,
+	   const double dx = 511.97,
+	   const double rn = 0,
+	   const double rx = 19000,
 	   const bool z = true,
 	   const double g = 0.85,
 	   const double f = 0.60,
 	   const double d = 10,
 	   const int c = 10000,
-	   const long i = 10000000) : Problem(r, s, n, x, z, g, f, d, c, i) {};
-  double problem(Individual subject) const;
+	   const long i = 10000000) : Problem(dn, dx, rn, rx, z, g, f, d, c, i) {};
+  double problem(const Individual * subject) const;
 };
 
 #endif /* _SCHWEFEL_PROBLEM_H_ */
