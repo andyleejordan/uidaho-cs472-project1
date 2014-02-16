898fb13c923e37e675a2f7520ff7c04bdf677e4e
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-02-16 15:46:03 -0800

    Re-factoring GA: using tournament selection
    - Typedef'd population as vector of individuals
    - Prepped for crossover, needs implementation
    - Fixed GA mutation for fitness parameter
    - Roulette wheel selection with discrete_dist deprecated
    - Arrays would take more programming, will need to profile

--- genetic_algorithm.cpp
+++ genetic_algorithm.cpp
@@ -10,49 +10,61 @@
 #include "genetic_algorithm.hpp"
 #include "random_generator.hpp"
 
-void Genetic::mutate(Individual & subject) const {
+const Individual Genetic::mutate(const Individual & subject) const {
   // unit Gaussian distribution for delta
+  Individual mutation = subject;
   normal_dist delta_dist(mean, stddev);
-  for (parameter & value : subject) {
-    parameter delta = delta_dist(rg.engine);
-    subject.mutate(value, value * delta);
-  }
+  for (parameter & gene : mutation)
+    mutation.mutate(gene, gene * delta_dist(rg.engine));
+  mutation.fitness = problem.fitness(mutation);
+  return mutation;
+}
+
+const Individual Genetic::selection(const Genetic::population & contestants) const {
+  // return best Individual from a set of contestants
+  return *std::max_element(contestants.begin(), contestants.end());
+}
+
+const Genetic::population Genetic::crossover(const Genetic::population & mates) const {
+  // return two offspring for two mates
+  return mates;
 }
 
 const Individual Genetic::solve() const {
-  // create initial population
-  std::array<Individual, pop_size> population;
-  population.fill(problem.potential());
-  Individual best;
-
-  for (long i = 0; i < problem.iterations; i++) {
-    std::array<Individual, pop_size> mating_pool;
-    mating_pool = population;
-
-    // calculate fitnesses of individuals in population
-    std::array<parameter, pop_size> weights;
-    for (int i = 0; i < pop_size; i++)
-      weights[i] = problem.fitness(population[i]);
-
-    // need location of best fitness to get corresponding population member
-    int max = std::distance(weights.begin(),
-			    std::max_element(weights.begin(), weights.end()));
-    best = population[max];
-    if (weights[max] > problem.goal) goto finished;
-    // std::cout << weights[max] << '\n';
-
-    // setup roulette wheel selection
-    std::discrete_distribution<> mate(weights.begin(), weights.end());
-
-    // selection and mutation stage
-    for (Individual & member : mating_pool) {
-      // select
-      member = population[mate(rg.engine)];
-      // mutate (GA mutate function receives by reference)
-      mutate(member);
+  while(true) {
+    // create initial population
+    population generation;
+    for (int i = 0; i < population_size; ++i)
+      generation.emplace_back(problem.potential());
+    // generations loop
+    for (long i = 0; i < problem.iterations; ++i) {
+      // find generation's best member (general form of selection)
+      const Individual best = selection(generation);
+      // terminating condition
+      if (best.fitness > problem.goal) return best;
+      // std::cout << best.fitness << '\n';
+      // selection and mutation stage
+      population offspring;
+      while(offspring.size() != population_size) {
+	// tournament selection of parents
+	population parents;
+	for (int i = 0; i < crossover_size; ++i) {
+	  population contestants;
+	  int_dist population_dist(0, population_size-1); // closed interval
+	  // create tournament of random members drawn from generation
+	  for (int i = 0; i < tournament_size; ++i)
+	    contestants.emplace_back(generation[population_dist(rg.engine)]);
+	  // select best member from each tournament
+	  parents.emplace_back(selection(contestants));
+	}
+	// crossover
+	population children = crossover(parents);
+	// add mutated children to offspring
+	for (const Individual child : children) offspring.emplace_back(mutate(child));
+      }
+      // replace generation with offspring
+      generation.swap(offspring);
     }
-    population = mating_pool;
+    std::cout << "Exhausted generations!\n";
   }
- finished:
-  return best;
 }
--- genetic_algorithm.hpp
+++ genetic_algorithm.hpp
@@ -12,14 +12,19 @@
 
 class Genetic: public Algorithm {
 private:
-  static const int pop_size = 4000;
+  static const int population_size = 4096;
+  static const int tournament_size = 2; // binary tournament selection
+  static const int crossover_size = 2; // binary crossover
+  typedef std::vector<Individual> population;
+  // settings for Gaussian distribution
   static const int mean = 0;
   static const int stddev = 2;
-
+  const Individual mutate(const Individual & subject) const;
+  const Individual selection(const population & contestants) const;
+  const population crossover(const population & mates) const;
 public:
-  Genetic(const Problem & p) : Algorithm(p) {};
+  Genetic(const Problem & p): Algorithm(p) {};
   const Individual solve() const;
-  void mutate(Individual & subject) const;
 };
 
 #endif /* _GENETIC_ALGORITHM_H_ */
