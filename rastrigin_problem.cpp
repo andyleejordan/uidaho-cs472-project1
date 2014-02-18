62719b00a9321e7a01cbc2846d8ccc6c3b096d8a
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-02-17 17:37:35 -0800

    Adding Rastrigin derived problem class

---
+++ rastrigin_problem.cpp
@@ -0,0 +1,15 @@
+/* Copyright 2014 Andrew Schwartzmeyer
+ *
+ * Source file for rastrigin problem derived class
+ */
+
+#include <cmath>
+
+#include "rastrigin_problem.hpp"
+
+parameter Rastrigin::problem(const Individual & subject) const {
+  parameter sum = 0;
+  for (const parameter & gene : subject)
+    sum += std::pow(gene, 2) - 10 * std::cos(2 * M_PI * gene);
+  return sum + 10 * subject.size();
+}
---
+++ rastrigin_problem.hpp
@@ -0,0 +1,26 @@
+/* Copyright 2014 Andrew Schwartzmeyer
+ *
+ * Header file for rastrigin problem derived class
+ */
+
+#ifndef _RASTRIGIN_PROBLEM_H_
+#define _RASTRIGIN_PROBLEM_H_
+
+#include "problem.hpp"
+#include "individual.hpp"
+
+class Rastrigin: public Problem {
+public:
+  Rastrigin(const long i = 1000000000,
+	    const parameter g = 0.999,
+	    const parameter f = 0.85,
+	    const parameter d = 0.1,
+	    const parameter h = 0.5,
+	    const int c = 10000): Problem(-5.12, 5.12,
+					  0, 900, true,
+					  i, g, f, d, h, c) {};
+
+  parameter problem(const Individual & subject) const;
+};
+
+#endif /* _RASTRIGIN_PROBLEM_H_ */
