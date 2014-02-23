7d9e311f4dabfff78a6f49c045bea2988c855fde
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-02-23 14:49:17 -0800

    Implementing most of CLI, can choose problem, iterations, goal
    - Uses boost/program_options
    - Algorithm choice still unimplemented
      - Can be done similarly to problem choice
    - Needs some cleanup, but works

--- main.cpp
+++ main.cpp
@@ -8,8 +8,11 @@
  * loop, random library)
  */
 
+#include <algorithm>
+#include <cassert>
 #include <cstdlib>
 #include <iostream>
+#include <memory>
 
 // boost
 #include <boost/program_options.hpp>
@@ -31,58 +34,77 @@
 #include "spherical_problem.hpp"
 
 namespace po = boost::program_options;
+using namespace std;
 
 int main(int argc, char * argv[]) {
   // setup program options
-  std::string po_algorithm;
-  std::vector<std::string> po_problem;
+  string po_algorithm;
+  vector<string> po_problems;
   long po_iterations;
   parameter po_goal;
   po::positional_options_description positionals;
   po::variables_map variables_map;
   po::options_description desc("Allowed options");
   desc.add_options()
-    ("help", "produce help message")
+    ("help,h", "produce help message")
     ("algorithm,a",
-     po::value<std::string>(&po_algorithm)->default_value("genetic"),
-     "choose algorithm")
+     po::value<string>(&po_algorithm)->default_value("Genetic"),
+     "choose algorithm, currently unimplemented")
     ("problem,p",
-     po::value<std::vector<std::string>>(&po_problem),
-     "choose problem(s)")
+     po::value<vector<string>>(&po_problems),
+     "choose problem(s), must be Title case, defaults to all")
     ("iterations,i",
      po::value<long>(&po_iterations)->default_value(256),
-     "set max iterations")
+     "set max iterations/generations")
     ("goal,g",
-     po::value<parameter>(&po_goal)->default_value(0), "set goal");
+     po::value<parameter>(&po_goal)->default_value(0),
+     "set goal, default will run until iterations is exhausted");
   try {
     po::store(po::command_line_parser(argc, argv).
 	      options(desc).positional(positionals).run(), variables_map);
     if (variables_map.count("help")) {
-      std::cout << "Basic Command Line Parameter App" << '\n'
-		<< desc << std::endl;
+      cout << "Search algorithms implemented in C++ by Andrew Schwartzmeyer\n\n"
+	   << "Code located at https://github.com/andschwa/uidaho-cs472-project1\n\n"
+	   << "Algorithms: Genetic, HillClimbing, and SimulatedAnnealing\n\n"
+	   << "Problems: Ackley, Griewangk, Rastrigin, Rosenbrock, Schwefel, and Spherical\n\n"
+	   << "Logs saved to logs/Problem.dat\n"
+	   << "GNUPlot settings in logs/Problem.p\n\n"
+	   << desc << endl;
       return 0;
     }
     po::notify(variables_map);
+    // add all problems if none specified
+    if (po_problems.size() == 0) {
+      po_problems.emplace_back("Ackley");
+      po_problems.emplace_back("Griewangk");
+      po_problems.emplace_back("Rastrigin");
+      po_problems.emplace_back("Rosenbrock");
+      po_problems.emplace_back("Schwefel");
+      po_problems.emplace_back("Spherical");
+    }
   }
-  catch(std::exception& e) {
-    std::cout << e.what() << '\n';
+  catch(exception& e) {
+    cout << e.what() << '\n';
     return 1;
   }
 
-  std::vector<Problem *> problems;
-  problems.emplace_back(new Ackley(po_iterations, po_goal));
-  problems.emplace_back(new Griewangk{po_iterations, po_goal});
-  problems.emplace_back(new Rastrigin{po_iterations, po_goal});
-  problems.emplace_back(new Rosenbrock{po_iterations, po_goal});
-  problems.emplace_back(new Schwefel{po_iterations, po_goal});
-  problems.emplace_back(new Spherical{po_iterations, po_goal});
+  vector<shared_ptr<Problem>> problems;
+  problems.emplace_back(shared_ptr<Problem>(new Ackley(po_iterations, po_goal)));
+  problems.emplace_back(shared_ptr<Problem>(new Griewangk(po_iterations, po_goal)));
+  problems.emplace_back(shared_ptr<Problem>(new Rastrigin(po_iterations, po_goal)));
+  problems.emplace_back(shared_ptr<Problem>(new Rosenbrock(po_iterations, po_goal)));
+  problems.emplace_back(shared_ptr<Problem>(new Schwefel(po_iterations, po_goal)));
+  problems.emplace_back(shared_ptr<Problem>(new Spherical(po_iterations, po_goal)));
 
-  for (auto problem : problems) {
+  for (const auto problem_name : po_problems) {
+    auto problem = *find_if(problems.begin(), problems.end(),
+			    [problem_name](const shared_ptr<Problem>& it)->bool
+			    { return it->compare(problem_name); });
+    assert(problem != nullptr);
     Genetic algorithm(*problem);
     const Individual solution = algorithm.solve();
-    std::cout << solution.represent() << '\n'
-	      << "Raw fitness: " << problem->fitness(solution) << '\n'
-	      << "Normalized fitness: " << problem->normal(solution) << std::endl;
+    cout << problem->represent() << solution.represent()
+	 << "Raw fitness: " << solution.fitness << '\n' << endl;
   }
   return 0;
 }
