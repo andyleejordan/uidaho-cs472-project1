0248c6ef5f26216fc2078f2e4818322ae0f55c5c
Andrew Schwartzmeyer <andrew@schwartzmeyer.com>
2014-03-09 20:36:17 -0700

    Adding aliases namespace and header for parameter

---
+++ src/aliases.hpp
@@ -0,0 +1,14 @@
+/* Copyright 2014 Andrew Schwartzmeyer
+ *
+ * Header file for common aliases
+ */
+
+#ifndef _ALIASES_H_
+#define _ALIASES_H_
+
+namespace aliases {
+  // this is biting me in the ass; probably going to replace all
+  typedef double parameter;
+}
+
+#endif /* _ALIASES_H_ */
